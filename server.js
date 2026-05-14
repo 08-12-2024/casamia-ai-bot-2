f356dde567d64692755ab4a4f64545c866ad4754
Casa Mia <casamiasrl2024@gmail.com>
2026-05-14 20:05:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,30 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+const VERIFY_TOKEN = "casamia_verify_token";
+
 app.get("/", (req, res) => {
   res.send("CASAMIA WhatsApp AI Bot attivo");
+});
+
+app.get("/webhook", (req, res) => {
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token === VERIFY_TOKEN) {
+    return res.status(200).send(challenge);
+  }
+
+  res.sendStatus(403);
+});
+
+app.post("/webhook", (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
 });
 
 app.listen(3000, () => {
